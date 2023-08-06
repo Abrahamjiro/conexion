0150c0bd3d8afc8d42951b9ea1384f7fe22ce349
Abrahamjiro <abrahamjiro2000@gmail.com>
2023-08-05 21:59:09 -0400

    Añadiendo el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './paginas/principal/principal.component';
+import { InsertarComponent } from './paginas/insertar/insertar.component';
+import { EliminarComponent } from './paginas/eliminar/eliminar.component';
+import { ListarComponent } from './paginas/listar/listar.component';
+import { ModificarComponent } from './paginas/modificar/modificar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    InsertarComponent,
+    EliminarComponent,
+    ListarComponent,
+    ModificarComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
